b58a9840c24c528898fc602fca18516a3120f6f1
Zneeky <arkanahmedov02@gmail.com>
2022-12-20 11:58:40 +0200

    bug fix

--- HTML Crawler 3.0/Data Structures/LinkStack.cs
+++ HTML Crawler 3.0/Data Structures/LinkStack.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HTML_Crawler_3._0.Data_Structures
 {
--- HTML Crawler 3.0/Data Structures/NLinkedList.cs
+++ HTML Crawler 3.0/Data Structures/NLinkedList.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HTML_Crawler_3._0.Data_Structures
 {
